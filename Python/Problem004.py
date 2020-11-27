cf111ba4cf8d8146b9d1418d490906e664618c56
NovusEdge <novusedge0@gmail.com>
2020-11-28 03:58:07 +0530

    Problem 4 in perl

--- Python/Problem004.py
+++ Python/Problem004.py
@@ -1,9 +1,15 @@
 from time import time
 
 isPall = lambda s : str(s)==str(s)[::-1]
-
+palls = []
 start = time()
-ans = max([i*j for (i,j) in enumerate(list(range(100,1001)),100) if isPall(i*j)])
+
+for i in range(999, 100, -1):
+    for j in range(999, 100, -1):
+        if isPall(i*j):
+            palls.append(i*j)
+            
+ans = max(palls)
 
 if __name__ == "__main__":
     print(f"\nAnswer: { ans }")
