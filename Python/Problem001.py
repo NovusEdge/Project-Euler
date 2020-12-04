8349dc9afb5a7da9dd4305019aed92727ef00cb1
NovusEdge <novusedge0@gmail.com>
2020-12-04 13:40:58 +0530

    Made some changes

--- Python/Problem001.py
+++ Python/Problem001.py
@@ -11,6 +11,8 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 start = time()
 ans = sum([i for i in range(1001) if (i % 3 == 0 or i % 5 == 0)])
 
+
+
 if __name__ == '__main__':
     print(f"\nAnswer:{ ans }")
     print(f"Time Taken:{ time() - start }\n")
