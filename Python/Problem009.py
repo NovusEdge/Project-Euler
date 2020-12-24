281df68ac50b929ce904edeccdc39ac5885618a6
NovusEdge <novusedge0@gmail.com>
2020-12-24 16:56:19 +0530

    ...

--- Python/Problem009.py
+++ Python/Problem009.py
@@ -5,7 +5,7 @@ from time import time
 Problem 9: https://projecteuler.net/problem=9
 
 A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
-                    
+
                     a**2 + b**2 = c**2
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
